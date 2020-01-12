ad3e9749373c028dd9a71ecb2a4fec2e9acd8ed8
rkr35 <rkr35@drexel.edu>
2020-01-12 10:20:17 -0500

    Create bl2_core and bl2_sdk_gen crates

---
+++ bl2_core/src/lib.rs
@@ -0,0 +1,8 @@
+use log::info;
+
+pub fn idle() {
+    use std::io::{self, Read};
+    info!("Idling. Press enter to continue.");
+    let mut sentinel = [0; 2];
+    let _ = io::stdin().read_exact(&mut sentinel);
+}
