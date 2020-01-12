45f4dbb252b3f818ff9c01e0709a5d6e0e54d15a
rkr35 <rkr35@drexel.edu>
2020-01-12 11:02:05 -0500

    MsgBox if DisableThreadLibraryCalls() fails

--- bl2_macros/src/lib.rs
+++ bl2_macros/src/lib.rs
@@ -10,7 +10,11 @@ use syn::parse_macro_input;
 pub fn main(input: OldTokenStream) -> OldTokenStream {
     let input = parse_macro_input!(input as TokenStream);
 
-    let on_attach = quote! {
+    let generated = quote! {
+        use winapi::{
+            shared::minwindef::{BOOL, DWORD, FALSE, HINSTANCE, LPVOID, TRUE},
+        };
+
         extern "system" fn on_attach(dll: LPVOID) -> DWORD {
             use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
             use winapi::{
@@ -45,9 +49,7 @@ pub fn main(input: OldTokenStream) -> OldTokenStream {
         
             0
         }
-    };
 
-    let dll_main = quote! {
         #[no_mangle]
         #[allow(non_snake_case)]
         extern "system" fn DllMain(dll: HINSTANCE, reason: DWORD, _: LPVOID)
@@ -58,28 +60,30 @@ pub fn main(input: OldTokenStream) -> OldTokenStream {
                     libloaderapi::DisableThreadLibraryCalls, 
                     processthreadsapi::CreateThread,
                     winnt::DLL_PROCESS_ATTACH,
+                    winuser::{MB_OK, MessageBoxW},
                 },
             };
 
             if reason == DLL_PROCESS_ATTACH {
                 unsafe {
-                    DisableThreadLibraryCalls(dll);
-                    CreateThread(null_mut(), 0, Some(on_attach), dll.cast(), 0,
-                                 null_mut());
+                    if DisableThreadLibraryCalls(dll) == 0 {
+                        use wchar::wch_c as w;
+                        let hwnd = null_mut();
+                        let text = w!("DisableThreadLibraryCalls failed.");
+                        let caption = w!("Error");
+                        let mb_type = MB_OK;
+                        MessageBoxW(hwnd, text.as_ptr(), caption.as_ptr(),
+                            mb_type);
+                        return FALSE;
+                    } else {
+                        CreateThread(null_mut(), 0, Some(on_attach), dll.cast(),
+                            0, null_mut());
+                    }
                 }
             }
 
             TRUE
         }
-    };
-
-    let generated = quote! {
-        use winapi::{
-            shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
-        };
-
-        #on_attach
-        #dll_main
     };
 
     generated.into()
