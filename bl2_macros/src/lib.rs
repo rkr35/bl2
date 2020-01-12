08605aa0bf6dde43e643fe7f78b1fe4eaffa96ca
rkr35 <rkr35@drexel.edu>
2020-01-12 10:20:01 -0500

    Initialize logging in bl2_macros::main

--- bl2_macros/src/lib.rs
+++ bl2_macros/src/lib.rs
@@ -9,25 +9,37 @@ use syn::parse_macro_input;
 #[proc_macro]
 pub fn main(input: OldTokenStream) -> OldTokenStream {
     let input = parse_macro_input!(input as TokenStream);
-    
-    let imports = quote! {
-        use core::ptr::null_mut;
-        use winapi::{
-            shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
-            um::{
-                libloaderapi::{DisableThreadLibraryCalls, 
-                               FreeLibraryAndExitThread},
-                processthreadsapi::CreateThread,
-                winnt::DLL_PROCESS_ATTACH,
-            },
-        };
-    };
 
     let on_attach = quote! {
         extern "system" fn on_attach(dll: LPVOID) -> DWORD {
-            #input
+            use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
+            use winapi::{
+                um::{
+                    consoleapi::AllocConsole,
+                    libloaderapi::FreeLibraryAndExitThread,
+                    wincon::FreeConsole,
+                }
+            };
 
             unsafe {
+                AllocConsole();
+            }
+            
+            println!("Allocated console.");
+
+            let filter = LevelFilter::Info;
+            let config = Config::default();
+            let mode = TerminalMode::Mixed;
+            if let Err(e) = TermLogger::init(filter, config, mode) {
+                eprintln!("Failed to initialize TermLogger: {}", e);
+                bl2_core::idle();
+            } else {
+                log::info!("Initialized logger.");
+                #input
+            }
+
+            unsafe {
+                FreeConsole();
                 FreeLibraryAndExitThread(dll.cast(), 0);
             }
         
@@ -40,6 +52,15 @@ pub fn main(input: OldTokenStream) -> OldTokenStream {
         #[allow(non_snake_case)]
         extern "system" fn DllMain(dll: HINSTANCE, reason: DWORD, _: LPVOID)
             -> BOOL {
+            use core::ptr::null_mut;
+            use winapi::{
+                um::{
+                    libloaderapi::DisableThreadLibraryCalls, 
+                    processthreadsapi::CreateThread,
+                    winnt::DLL_PROCESS_ATTACH,
+                },
+            };
+
             if reason == DLL_PROCESS_ATTACH {
                 unsafe {
                     DisableThreadLibraryCalls(dll);
@@ -52,9 +73,11 @@ pub fn main(input: OldTokenStream) -> OldTokenStream {
         }
     };
 
-
     let generated = quote! {
-        #imports
+        use winapi::{
+            shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
+        };
+
         #on_attach
         #dll_main
     };
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,7 @@
 #![warn(clippy::pedantic)]
 
 use bl2_macros::main;
-use winapi::{
-    um::{
-        consoleapi::AllocConsole,
-        wincon::FreeConsole,
-    },
-};
-
-fn idle() {
-    use std::io::{self, Read};
-    // info!("Idling. Press enter to continue.");
-
-    let mut sentinel = [0; 2];
-
-    match io::stdin().read_exact(&mut sentinel) {
-        _ => (),
-    }
-}
 
 main! {
-    unsafe {
-        AllocConsole();
-    }
-    
-    println!("Hey, got this .DLL injected!");
-    idle();
-
-    unsafe {
-        FreeConsole();
-    }
+    bl2_core::idle();
 }
