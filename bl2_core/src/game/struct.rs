c137162d6d4d6d85eb8e102ee38514dbe8ac00b8
rkr35 <rkr35@drexel.edu>
2020-03-15 15:29:29 -0400

    Add Struct

--- bl2_core/src/game/struct.rs
+++ bl2_core/src/game/struct.rs
@@ -1,10 +1,26 @@
+use core::ops::{Deref, DerefMut};
 use super::Field;
 
 #[repr(C)]
 pub struct Struct<'a> {
-    pad0: [u8; 0x8],
-    super_field: Option<&'a Field<'a>>,
-    children: Option<&'a Field<'a>>,
-    property_size: u16,
+    field: Field<'a>,
+    pad0: u64,
+    pub super_field: Option<&'a Struct<'a>>,
+    pub children: Option<&'a Field<'a>>,
+    pub property_size: u16,
     pad1: [u8; 0x3a],
+}
+
+impl<'a> Deref for Struct<'a> {
+    type Target = Field<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.field
+    }
+}
+
+impl<'a> DerefMut for Struct<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.field
+    }
 }
