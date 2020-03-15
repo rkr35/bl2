3d7023c37be3d30190a72be391a2458a311defd8
rkr35 <rkr35@drexel.edu>
2020-03-15 15:29:19 -0400

    Add Field

--- bl2_core/src/game/field.rs
+++ bl2_core/src/game/field.rs
@@ -1,4 +1,22 @@
+use core::ops::{Deref, DerefMut};
+use super::Object;
+
 #[repr(C)]
 pub struct Field<'a> {
+    object: Object<'a>,
     next: Option<&'a Field<'a>>,
+}
+
+impl<'a> Deref for Field<'a> {
+    type Target = Object<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.object
+    }
+}
+
+impl<'a> DerefMut for Field<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.object
+    }
 }
