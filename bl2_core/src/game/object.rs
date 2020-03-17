e332d90519fec2362afccd6fae0a0b63eb186365
rkr35 <rkr35@drexel.edu>
2020-03-17 10:12:46 -0400

    Convert tabs to spaces

--- bl2_core/src/game/object.rs
+++ bl2_core/src/game/object.rs
@@ -9,14 +9,14 @@ use super::Struct;
 class UObject
 {
 public:
-	FPointer		VTableObject;
-	char			UnknownData00[0x1C];
-	uint32_t		InternalIndex;
-	char			UnknownData01[0x04];
-	UObject*		Outer;
-	FName			Name;
-	UObject*		Class;
-	char			UnknownData02[0x04];
+    FPointer		VTableObject;
+    char			UnknownData00[0x1C];
+    uint32_t		InternalIndex;
+    char			UnknownData01[0x04];
+    UObject*		Outer;
+    FName			Name;
+    UObject*		Class;
+    char			UnknownData02[0x04];
 };
 
 */
@@ -35,67 +35,67 @@ pub struct Object<'a> {
 
 impl<'a> Object<'a> {
     pub fn name<'n>(&self, global_names: &'n Names) -> Option<&'n str> {
-		self.entry(global_names)?.to_str().ok()
-	}
+        self.entry(global_names)?.to_str().ok()
+    }
 
     pub fn entry<'n>(&self, global_names: &'n Names) -> Option<&'n Entry> {
         self.name.entry(global_names)
     }
 
     pub fn full_name<'n>(&self, global_names: &'n Names) -> String {
-		let outers = {
-			let mut v: Vec<_> = self
-				.outer_iter()
-				.map(|o| o.name(global_names).unwrap_or("!OUTER_UNKNOWN!"))
-				.collect();
-			v.reverse();
-			v.join(".")
-		};
+        let outers = {
+            let mut v: Vec<_> = self
+                .outer_iter()
+                .map(|o| o.name(global_names).unwrap_or("!OUTER_UNKNOWN!"))
+                .collect();
+            v.reverse();
+            v.join(".")
+        };
 
-		let class = self
-			.class
-			.and_then(|c| c.name(global_names))
-			.unwrap_or("!CLASS_UNKNOWN!");
-			
-		let self_name = self
-			.name(global_names)
-			.unwrap_or("!SELF_UNKNOWN!");
+        let class = self
+            .class
+            .and_then(|c| c.name(global_names))
+            .unwrap_or("!CLASS_UNKNOWN!");
+            
+        let self_name = self
+            .name(global_names)
+            .unwrap_or("!SELF_UNKNOWN!");
 
-		if outers.is_empty() {
-			format!("{} {}", class, self_name)
-		} else {
-			format!("{} {}.{}", class, outers, self_name)
-		}
-	}
-	
-	pub fn package(&self) -> Option<&Object> {
-		self.outer_iter().last()
-	}
+        if outers.is_empty() {
+            format!("{} {}", class, self_name)
+        } else {
+            format!("{} {}.{}", class, outers, self_name)
+        }
+    }
+    
+    pub fn package(&self) -> Option<&Object> {
+        self.outer_iter().last()
+    }
 
-	fn outer_iter(&self) -> impl Iterator<Item = &Object> {
-		iter::successors(self.outer, |o| o.outer)
-	}
+    fn outer_iter(&self) -> impl Iterator<Item = &Object> {
+        iter::successors(self.outer, |o| o.outer)
+    }
 
-	pub fn class_iter(&self) -> impl Iterator<Item = &Struct> {
-		iter::successors(self.class, |o| o.super_field)
-	}
+    pub fn class_iter(&self) -> impl Iterator<Item = &Struct> {
+        iter::successors(self.class, |o| o.super_field)
+    }
 
-	pub fn is(&self, class: &Object) -> bool {
-		self.class_iter().any(|c| c.eq(class))
-	}
+    pub fn is(&self, class: &Object) -> bool {
+        self.class_iter().any(|c| c.eq(class))
+    }
 }
 
 impl<'a> PartialEq for Object<'a> {
-	fn eq(&self, other: &Self) -> bool {
-		ptr::eq(self, other)
-	}
+    fn eq(&self, other: &Self) -> bool {
+        ptr::eq(self, other)
+    }
 }
 
 impl<'a> Eq for Object<'a> {}
 
 impl<'a> Hash for Object<'a> {
-	fn hash<H: Hasher>(&self, state: &mut H) {
-		let pointer = self as *const _ as usize;
-		pointer.hash(state);
-	}
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let pointer = self as *const _ as usize;
+        pointer.hash(state);
+    }
 }
