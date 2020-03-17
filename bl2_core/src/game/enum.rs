53d2fbcf5e346c431e9bfa641aa22f04b8cb4d8a
rkr35 <rkr35@drexel.edu>
2020-03-16 20:17:28 -0400

    Add Enum

--- bl2_core/src/game/enum.rs
+++ bl2_core/src/game/enum.rs
@@ -1,6 +1,40 @@
-use super::{Array, Name};
+use core::ops::{Deref, DerefMut};
+use crate::globals::Names;
+use super::{Array, Field, Name};
 
 #[repr(C)]
 pub struct Enum<'a> {
-    names: Array<'a, Name>,
+    field: Field<'a>,
+    variants: Array<'a, Name>,
+}
+
+impl<'a> Enum<'a> {
+    pub fn variants<'n>(&self, global_names: &'n Names)
+        -> Vec<&'n str> {
+        self
+            .variants
+            .iter()
+            .map(|n| n
+                .entry(global_names)
+                .map_or(
+                    "UNKNOWN VARIANT NAME",
+                    |e| e.to_str().unwrap_or("NON UTF-8 VARIANT NAME")
+                )
+            )
+            .collect()
+    }
+}
+
+impl<'a> Deref for Enum<'a> {
+    type Target = Field<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.field
+    }
+}
+
+impl<'a> DerefMut for Enum<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.field
+    }
 }
