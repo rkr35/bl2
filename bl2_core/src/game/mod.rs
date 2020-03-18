c20c146d21fdaf0363a11fadd04f9fcce9ff0325
rkr35 <rkr35@drexel.edu>
2020-03-17 20:42:14 -0400

    Sort game modules

--- bl2_core/src/game/mod.rs
+++ bl2_core/src/game/mod.rs
@@ -1,20 +1,20 @@
+mod array;
+pub use array::Array;
+
+mod field;
+pub use field::Field;
+
 mod name;
 pub use name::{Entry, Name};
 
-mod array;
-pub use array::Array;
+mod object;
+pub use object::Object;
 
 mod r#const;
 pub use r#const::Const;
 
 mod r#enum;
 pub use r#enum::Enum;
-
-mod field;
-pub use field::Field;
-
-mod object;
-pub use object::Object;
 
 mod r#struct;
 pub use r#struct::Struct;
